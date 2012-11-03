5aeb6eed82e208024d4a4634bf59c5ec97a911e5
Kalinichenko <le.cactus@mail.ru>
2012-11-03 22:13:49 +1000

    изменено отсеивание по рейтингу
    момент отсеивания пользователей с рейтингом меньше 100 вынесен за старый
    запрос

--- top100.sql
+++ top100.sql
@@ -1,24 +1,27 @@
-select all_gpms_and_rayting.gpm_id, all_gpms_and_rayting.rayting from
+select final_result.gpm_id, final_result.rayting from
 (
-	-- select all gpms and it's raiting
-		select last_profiles.gpm_id as gpm_id, rayting/count_posts as rayting from
-		(
-		-- select profiles which were updated last
-			SELECT gpm as gpm_id, count(gpm) as count_posts
-			FROM post 
-			group by post.gpm
-		) last_profiles
-		left join
-		-- select gpm id and his post-weight
-		(
-			select post.gpm as gpm_id, (sum(nPlusOne)*1 + sum(nComment)*5 + sum(nResharers)*20) as rayting 
-			from post
-			group by post.gpm
-		) as post_rayting
-		on last_profiles.gpm_id = post_rayting.gpm_id
-		order by rayting desc
-) as all_gpms_and_rayting
-left join blacklist
-on blacklist.id = all_gpms_and_rayting.gpm_id -- select thouse gpms, who are not in black list
-where blacklist.id is null
-limit 100;
+	select all_gpms_and_rayting.gpm_id as gpm_id, all_gpms_and_rayting.rayting as rayting from
+	(
+		-- select all gpms and it's raiting
+			select last_profiles.gpm_id as gpm_id, rayting/count_posts as rayting from
+			(
+			-- select profiles which were updated last
+				SELECT gpm as gpm_id, count(gpm) as count_posts
+				FROM post 
+				group by post.gpm
+			) last_profiles
+			left join
+			-- select gpm id and his post-weight
+			(
+				select post.gpm as gpm_id, (sum(nPlusOne)*1 + sum(nComment)*5 + sum(nResharers)*20) as rayting 
+				from post
+				group by post.gpm
+			) as post_rayting
+			on last_profiles.gpm_id = post_rayting.gpm_id
+			order by rayting desc
+	) as all_gpms_and_rayting
+	left join blacklist
+	on blacklist.id = all_gpms_and_rayting.gpm_id -- select thouse gpms, who are not in black list
+	where blacklist.id is null
+) as final_result
+limit 0, 100;
